e8def714708a8b4a76f9210e5ce9aa0102049008
Dion Hulse <contact@dd32.id.au>
2021-02-25 12:35:40 +1000

    Initial configuration

---
+++ wp-content/mu-plugins/stubs.php
@@ -0,0 +1,5 @@
+<?php
+/**
+ * Plugin Name: WordPress.org Stubs
+ * Description: This plugin contains any stubs for other WordPress.org systems required to make the Theme Directory work.
+ */
---
+++ wp-content/mu-plugins/wordpressorg.php
@@ -0,0 +1,4 @@
+<?php
+/**
+ * Plugin Name: WordPress.org opinionated settings required for the environment.
+ */
