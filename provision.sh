5fb852d4545ccc15b8dd541ae7d9c918cbff53c5
Dion Hulse <contact@dd32.id.au>
2022-06-09 10:19:49 +1000

    Add the wporg-mu-plugins repo, for the new WordPress.org header. Fixes #5

--- provision.sh
+++ provision.sh
@@ -7,3 +7,6 @@
 
 # We need a checkout of theme-check
 [[ -d wp-content/plugins/theme-check ]] || git clone https://github.com/wordpress/theme-check wp-content/plugins/theme-check
+
+# we need the wporg-mu-plugins for headers.
+[[ -d wporg-mu-plugins.git ]] || git clone --branch build https://github.com/WordPress/wporg-mu-plugins wporg-mu-plugins.git
---
+++ wp-content/mu-plugins/pub.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Plugin Name: WordPress.org MU-Plugins.
+ * Description: Loads the wporg-mu-plugins plugins.
+ */
+
+include_once __DIR__ . '/pub/loader.php';
